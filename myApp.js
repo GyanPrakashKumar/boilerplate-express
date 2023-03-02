d691ba82adfbac4ee842f89886be45e407835c05
Gyan-Prakash-Ku <15365244-Gyan-Prakash-Ku@users.noreply.replit.com>
2023-03-02 19:18:18 +0000

    json path added

--- myApp.js
+++ myApp.js
@@ -2,16 +2,18 @@ let express = require('express');
 let app = express();
 console.log("Hello World")
 
+app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => {
     let path = __dirname + '/views/index.html';
     res.sendFile(path);
 });
 
-
-
-
-
-
+app.get("/json", (req, res) => {
+    res.json({
+        message:"Hello json"
+    });
+});
 
 
 
