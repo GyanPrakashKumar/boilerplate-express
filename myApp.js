3e6164350ee30f1b3dad02600ce91ab47eceb8ba
Gyan-Prakash-Ku <15365244-Gyan-Prakash-Ku@users.noreply.replit.com>
2023-03-09 17:19:44 +0000

    done

--- myApp.js
+++ myApp.js
@@ -1,7 +1,6 @@
 let express = require("express");
 let app = express();
 require("dotenv").config();
-let bodyParser = require("body-parser");
 
 app.use("/public", express.static(__dirname + "/public"));
 
@@ -10,7 +9,6 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
@@ -23,35 +21,5 @@ app.get("/json", (req, res) => {
   }
   return res.status(200).json({ message: message });
 });
-
-app.get(
-  "/now",
-  (req, res, next) => {
-    req.time = new Date().toString();
-    next();
-  },
-  (req, res) => {
-    res.send({
-      time: req.time,
-    });
-  }
-);
-
-app.get("/:word/echo", (req, res) => {
-  const word = req.params.word;
-  res.json({ echo: word });
-});
-
-app.get("/name", (req, res) => {
-  const firstName = req.query.first;
-  const lastName = req.query.last;
-  res.json({ name: `${firstName} ${lastName}` });
-});
-
-app.post("/name", (req, res) => {
-  const firstName = req.body.first;
-  const lastName = req.body.last;
-  res.json({ name: `${firstName} ${lastName}` });
-})
 
 module.exports = app;
