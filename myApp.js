37e9e6aa605168160f1769e3b7ed7280c27f723a
Gyan-Prakash-Ku <15365244-Gyan-Prakash-Ku@users.noreply.replit.com>
2023-03-09 17:27:11 +0000

    done

--- myApp.js
+++ myApp.js
@@ -4,8 +4,8 @@ require("dotenv").config();
 
 // app.use("/public", express.static(__dirname + "/public"));
 
-app.use(function (req, res, next) {
-  console.log(req.method + " " + req.path + " - " + req.ip);
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
   next();
 });
 
