746a6a6fd4006adcc3a952999f7a87e6c29a4ee5
Gyan-Prakash-Ku <15365244-Gyan-Prakash-Ku@users.noreply.replit.com>
2023-03-09 17:18:07 +0000

    Basic Node and Express Module Finished

--- myApp.js
+++ myApp.js
@@ -1,36 +1,57 @@
-let express = require('express');
+let express = require("express");
 let app = express();
+require("dotenv").config();
+let bodyParser = require("body-parser");
 
-// Root level logger middleware
+app.use("/public", express.static(__dirname + "/public"));
+
 app.use(function (req, res, next) {
   console.log(req.method + " " + req.path + " - " + req.ip);
   next();
 });
 
-app.use("/public", express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-    let path = __dirname + '/views/index.html';
-    res.sendFile(path);
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/json", (req, res) => {
+  let message = "Hello json";
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    return res.json({ message: message.toUpperCase() });
+  }
+  return res.status(200).json({ message: message });
+});
 
-app.get('/json', (req, res) => {
-    if (process.env['MESSAGE_STYLE'] == "uppercase") {
-      res.json({
-          "message": "Hello json".toUpperCase()
-      });
-    } else {
-    res.json({
-        "message": "Hello json"
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.send({
+      time: req.time,
     });
-    }
+  }
+);
+
+app.get("/:word/echo", (req, res) => {
+  const word = req.params.word;
+  res.json({ echo: word });
 });
 
+app.get("/name", (req, res) => {
+  const firstName = req.query.first;
+  const lastName = req.query.last;
+  res.json({ name: `${firstName} ${lastName}` });
+});
 
-
-
-
-
+app.post("/name", (req, res) => {
+  const firstName = req.body.first;
+  const lastName = req.body.last;
+  res.json({ name: `${firstName} ${lastName}` });
+})
 
 module.exports = app;
