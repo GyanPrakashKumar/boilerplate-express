77ffe790afa613fd9761258f6206b2178253a854
Gyan-Prakash-Ku <15365244-Gyan-Prakash-Ku@users.noreply.replit.com>
2023-03-09 17:28:32 +0000

    done

--- myApp.js
+++ myApp.js
@@ -1,25 +1,24 @@
-let express = require("express");
+let express = require('express');
 let app = express();
-require("dotenv").config();
-
-// app.use("/public", express.static(__dirname + "/public"));
-
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.path} - ${req.ip}`);
   next();
 });
 
-
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+  res.sendFile('/views/index.html');
 });
-
 app.get("/json", (req, res) => {
-  let message = "Hello json";
-  if (process.env.MESSAGE_STYLE === "uppercase") {
-    return res.json({ message: message.toUpperCase() });
-  }
-  return res.status(200).json({ message: message });
+  if (process.env.MESSAGE_STYLE === "uppercase")
+    res.json({
+      message: "Hello json".toUpperCase()
+    });
+  else
+    res.json({
+      message: "Hello json"
+    });
 });
+// Assets at the /public route
+app.use('/public', express.static(__dirname + '/public'));
 
 module.exports = app;
